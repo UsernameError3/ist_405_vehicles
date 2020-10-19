6879ffaa415e7c30662c367bb5ef4c5d8d78b37b
Alex Fleming <alexfleming421@gmail.com>
2020-10-19 00:01:38 -0500

    changes

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ Developed:  10/11/20
 Tested:     10/11/20
 ******************************************************************************/
 
-/*
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
+
 
 
 // List all cars
