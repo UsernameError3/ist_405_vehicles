f12195d3d292088324a175fb573fbebc58573772
Alex Fleming <alexfleming421@gmail.com>
2020-10-18 22:11:55 -0500

    changes

--- add_car.php
+++ add_car.php
@@ -10,46 +10,43 @@ Developed:  10/11/20
 Tested:     10/11/20
 ******************************************************************************/
 
-// Get the car form data
-$car_make = filter_input(INPUT_POST, 'car_Make');
-$car_model = filter_input(INPUT_POST, 'car_Model');
-$car_color = filter_input(INPUT_POST, 'car_Color');
-$car_year = filter_input(INPUT_POST, 'car_Year', FILTER_VALIDATE_INT);
-$car_price = filter_input(INPUT_POST, 'car_Price', FILTER_VALIDATE_FLOAT);
+function addCar() {
 
-// Validate inputs
-if ($car_make == null || $car_make == false || 
-    $car_model == null || $car_model == false || 
-    $car_color == null || $car_color == false || 
-    $car_year == null || $car_year == false || 
-    $car_price == null || $car_price == false) {
-        $error_message = "Invalid car data. Check all fields and try again.";
-        include('db_error.php');
-} else {
-    require_once('db_conn.php');
+    // Get the car form data
+    $car_make = filter_input(INPUT_POST, 'car_Make');
+    $car_model = filter_input(INPUT_POST, 'car_Model');
+    $car_color = filter_input(INPUT_POST, 'car_Color');
+    $car_year = filter_input(INPUT_POST, 'car_Year', FILTER_VALIDATE_INT);
+    $car_price = filter_input(INPUT_POST, 'car_Price', FILTER_VALIDATE_FLOAT);
 
-    // Add car to the database  
-    $queryAddCar = 'INSERT INTO cars 
-                        (car_make, car_model, car_color, car_year, car_price)
-                    VALUES 
-                        (:car_make, :car_model, :car_color, :car_year, :car_price)';
-
-    $db_add_process = $db->prepare($queryAddCar);
-    $db_add_process->bindValue(':car_make', $car_make);
-    $db_add_process->bindValue(':car_model', $car_model);
-    $db_add_process->bindValue(':car_color', $car_color);
-    $db_add_process->bindValue(':car_year', $car_year);
-    $db_add_process->bindValue(':car_price', $car_price);
-    $db_add_process->execute();
-    $db_add_process->closeCursor();
-
-    if ($pinned == true) {
-        include('pinned.php');
-    } elseif ($pinned == false) {
-        include('snippets.php');
+    // Validate inputs
+    if ($car_make == null || $car_make == false || 
+        $car_model == null || $car_model == false || 
+        $car_color == null || $car_color == false || 
+        $car_year == null || $car_year == false || 
+        $car_price == null || $car_price == false) {
+            $error_message = "Invalid car data. Check all fields and try again.";
+            include('db_error.php');
     } else {
-        include('index.php');
-    }
+        require_once('db_conn.php');
+
+        // Add car to the database  
+        $queryAddCar = 'INSERT INTO cars 
+                            (car_make, car_model, car_color, car_year, car_price)
+                        VALUES 
+                            (:car_make, :car_model, :car_color, :car_year, :car_price)';
+
+        $db_add_process = $db->prepare($queryAddCar);
+        $db_add_process->bindValue(':car_make', $car_make);
+        $db_add_process->bindValue(':car_model', $car_model);
+        $db_add_process->bindValue(':car_color', $car_color);
+        $db_add_process->bindValue(':car_year', $car_year);
+        $db_add_process->bindValue(':car_price', $car_price);
+        $db_add_process->execute();
+        $db_add_process->closeCursor();
+
+        header("Location: index.php");
+
 }
 
 ?>
@@ -79,8 +76,10 @@ if ($car_make == null || $car_make == false ||
             <label>Price:</label><br>
             <input type="text" name="car_Price"><br>
             <label> </label><br>
-            <input type="submit" name="add" value="Add Car"><br>
+            <input type="submit" name="add" action="addCar()" value="Add Car"><br>
         </form>
+        <!-- Add Car -->
+        <p><a href="index.php">Return to Main Page</a></p>
     </main>
 </body>
 
