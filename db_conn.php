e372143040bfcfe829dbf3e697c559532ec3814d
Alex Fleming <alexfleming421@gmail.com>
2020-10-18 20:45:29 -0500

    changes

--- db_conn.php
+++ db_conn.php
@@ -10,7 +10,7 @@ Developed:  10/11/20
 Tested:     10/11/20
 ******************************************************************************/
 
-$dsn = 'mysql:host=127.0.0.1;db_name=vehicles';
+$dsn = 'mysql:host=127.0.0.1;dbname=vehicles';
 $username = 'root';
 $password = 'abc123';
 
